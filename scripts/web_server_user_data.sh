bd8ccc07a5cdf2b418f299cd73f91ff885f82a5d
Ignacio Sagues <isagues@itba.edu.ar>
2022-06-11 01:54:50 -0300

    scripts

--- scripts/web_server_user_data.sh
+++ scripts/web_server_user_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum update -y
 amazon-linux-extras install -y nginx1
+wget https://raw.githubusercontent.com/isagues/terraform-demo/main/scripts/api.conf
 systemctl start nginx
 systemctl enable nginx
