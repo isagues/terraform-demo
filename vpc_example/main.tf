ca8e60b3b573d82ad65b26feb41e1e85c6632c51
Faustino <fpannunzio@itba.edu.ar>
2022-06-12 23:29:36 -0300

    comments

--- vpc_example/main.tf
+++ vpc_example/main.tf
@@ -8,19 +8,3 @@ module "vpc" {
     cidr_block = "10.0.0.0/16"
     zones_count = 3
 }
-
-# resource "aws_key_pair" "redes_key" {
-#   key_name   = "key"
-#   public_key = file("C:/Users/faust/.ssh/id_rsa.pub")
-# }
-
-# module "bastion" {
-#     source = "../aws/modules/bastion"
-
-#     vpc_id        = module.vpc.vpc_id
-#     subnets       = module.vpc.public_subnets_ids
-#     key_name      = "key"
-#     ami           = "ami-0022f774911c1d690"
-#     my_ips        = ["200.123.140.195/32", "181.29.41.98/32"]
-#     instance_type = "t2.micro"
-# }
